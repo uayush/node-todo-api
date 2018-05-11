e9434f37da335e55c424f6a6bdeeda73b9010ddb
uayush <38280214+uayush@users.noreply.github.com>
2018-05-11 11:43:55 +0530

    Update mongoose.js

--- server/DB/mongoose.js
+++ server/DB/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://localhost:27017/TodoApp'); // Creation of the database
 
 module.exports = {mongoose}; //es6
